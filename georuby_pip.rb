a11ab635ae0c7805bbee9975c6fbc2f31040c3bc
nox <nox@sumavisos.com>
2010-10-25 10:58:04 -0300

    GeoRuby module extension and test case

--- georuby_pip.rb
+++ georuby_pip.rb
@@ -1,18 +1,39 @@
 $LOAD_PATH << File.expand_path(File.dirname(__FILE__))
 require "point_in_polygon"
 
-module GeoRuby
-  module SimpleFeatures
-    class Polygon
-      def contains?(point)
-        pps = self.rings.map(&:points).map{|ps| ps.map{|p| [p.x, p.y]}}
-        if pps.size == 1
-          PointInPolygon.point_in_polygon? pps.first, point.x, point.y
-        else
-          # done
-          raise NotImplementedError
-        end
-      end
-    end
-  end
+module GeoRuby::SimpleFeatures
+
+	class LinearRing
+		def contains_point?(point)
+			pps = self.points.map {|p| [p.x, p.y]}
+			PointInPolygon.point_in_polygon? pps, point.x, point.y
+		end
+	end
+
+	class Polygon
+		def contains_point?(point)
+			return false if self.rings.empty?
+
+			outer_ring = self.rings.first
+			if rings.size == 1
+				return outer_ring.contains_point?(point)
+			else
+				inner_rings = self.rings[1..-1]
+				if outer_ring.contains_point?(point)
+					return inner_rings.all? { |r| not r.contains_point?(point) }
+				else
+					return false
+				end
+			end
+		end
+	end
+
+	class MultiPolygon
+		def contains_point?(point)
+			return false if self.geometries.empty?
+
+			self.geometries.any? { |p| p.contains_point?(point) }
+		end
+	end
+
 end
